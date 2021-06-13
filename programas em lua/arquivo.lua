e9406b4930174b474aee2eeb1229d21559a2b9fb
Luiz fabiano silva samuel <85593514+LuizGafanhotos@users.noreply.github.com>
2021-06-12 23:23:29 -0300

    Agora sim evoluimos
    AEEEE EVOLUIMOS NA LINGUAGEM LUA

---
+++ programas em lua/arquivo.lua
@@ -0,0 +1,14 @@
+print("Qual e o seu nome?")
+nome = io.read()
+print(nome .. " que belo nome esse seu.")
+umVetor = -31
+print("O valor ".. umVetor)
+
+nomeal = "Caixota D\'agua"
+print(nome)
+
+rer = "Estamos \"rico\" "
+print(rer)
+
+converter = tostring(31)
+print("O valor convertido: ".. converter)
