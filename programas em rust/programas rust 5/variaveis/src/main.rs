363c2e1c5c052e0c003557a27eac6049dee6b1f8
unknown <luizfabiano.13lfww@gmail.com>
2021-06-13 22:04:39 -0300

    Arquivos rust

--- programas em rust/programas rust 5/variaveis/src/main.rs
+++ programas em rust/programas rust 5/variaveis/src/main.rs
@@ -1,8 +1,28 @@
 fn main() {
+    //Tipos de variaveis
+    //-----------------
+    /*flutuante
+    let xe = 2.0;
+    let ya: f32 = 3.0;
+    */
+    //-----------------
+
+    /*inteira
+    let xox = 41;
+    let xax: i32 = 51;
+    */
+    //---------------
+    /*caractere
+    let letra =  'Z';
+    let pal = 'z';
+    let gato_do_amor = '😻';
+    */
+    //--------------
+
+    //Variaves mutaveis e imutáveis
     let mut x = 5;
     println!("O primeiro valor de x e {}", x);
     x = 6;
     println!("agora O mesmo valor de x e {}", x);
-
 
 }
---
+++ programas em rust/programas rust 6/tupla/target/debug/deps/tupla.d
@@ -0,0 +1,5 @@
+C:\Users\renata\Documents\GitHub\MeuDados\Python_Software\programas em rust\programas rust 6\tupla\target\debug\deps\tupla.exe: src\main.rs
+
+C:\Users\renata\Documents\GitHub\MeuDados\Python_Software\programas em rust\programas rust 6\tupla\target\debug\deps\tupla.d: src\main.rs
+
+src\main.rs:
