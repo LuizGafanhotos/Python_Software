7771ab38d7308bc40b002a3a95999c0e25c7711d
unknown <luizfabiano.13lfww@gmail.com>
2021-06-15 15:37:59 -0300

    Rust

--- programas em rust/programa rust 8/Funcoes/src/main.rs
+++ programas em rust/programa rust 8/Funcoes/src/main.rs
@@ -1,12 +1,29 @@
+fn linha(){
+    let line = "-".repeat(30);
+    println!("{}",line);
+}
+
+fn cinco() -> i32{
+    5
+}
+
 fn main() {
+    
+    println!("Uma funcao com valores de retorno");
+    let func = cinco();
+    println!("Ta ai, o valor de func e {}", func);
+    linha();
+
     println!("Funcao ola mundo com valor");
-    println!("--------------------");
+    linha();
     println!("Ola mundo");
     funcao();
-    println!("------------------");
+
+    linha();
     println!("Agora funcao com parametro e valor??");
     parametro(4);
-    println!("-------------------");
+    linha();
+
 
 }
 
