1da0c8107d19d0ddb481f9d8859cf49da54628ea
unknown <luizfabiano.13lfww@gmail.com>
2021-06-27 12:46:29 -0300

    O inicio de um sistema

--- programas em python/Tkintor.py
+++ programas em python/Tkintor.py
@@ -2,5 +2,8 @@ from tkinter import *
 
 janelito = Tk()
 
+lb = Label(janelito, text="Ola mundo lindo e maravilhoso")
+lb.place(x=300, y=330)
+
 janelito.geometry("200x200+300+300")
 janelito.mainloop()
