4f7e9c606b6d7ee7fae6c6f3e46a451946e814d8
LuizGafanhotos <luizfabiano.13lfww@gmail.com>
2021-07-20 17:55:07 -0300

    damião gentili

--- programas em rust/oller/src/main.rs
+++ programas em rust/oller/src/main.rs
@@ -1,6 +1,11 @@
 fn main() {
-    let mut s = String::from("Ola");
+    // let mut s = String::from("Ola");
 
-    s.push_str(", mundo!!");
-    println!("{}",s);
+    // s.push_str(", mundo!!");
+    // println!("{}",s);
+
+    let s1 = String::from("Ola");
+    let s2 = s1.clone();
+
+    println!("s1 = {}, s2 = {}", s1, s2);
 }
