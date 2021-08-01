37ca5e42407a0dad2789bb78cf081b0e9971ef32
LuizGafanhotos <luizfabiano.13lfww@gmail.com>
2021-08-01 18:53:26 -0300

    Afirmagem

---
+++ programas em python/modulação/base.py
@@ -0,0 +1,3 @@
+import modulagem
+
+modulagem.canal()
---
+++ programas em python/modulação/modulagem.py
@@ -0,0 +1,9 @@
+def canal():
+    print("Curso em video")
+
+jogador = {
+    "nome": "Bruce",
+    "pontos": 100,
+    "energia": 75,
+    "time": 1
+}
