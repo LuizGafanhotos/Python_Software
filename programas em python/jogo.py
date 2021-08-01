b7053aad4133847ef995fd9c85064e77a9b2542f
LuizGafanhotos <luizfabiano.13lfww@gmail.com>
2021-08-01 18:11:49 -0300

    O jogo refeito

--- programas em python/jogo.py
+++ programas em python/jogo.py
@@ -1,148 +1,165 @@
 import os
 import random
-from colorama import Fore, Back, Style
+from colorama import Fore
 
 jogarNovamente = "s"
 jogadas = 0
-quemJoga = 2
+quemJoga = 2 #1 = CPU | #2 = Jogador
 maxJogadas = 9
-vit = "n"
-velha = [
-    [" ", " ", " "],
-    [" ", " ", " "],
-    [" ", " ", " "]
+vencedor = ""
+matriz = [[" ", " ", " "],[" ", " ", " "],[" ", " ", " "]]
 
-]
 def tela():
-    linha = "-"
-    global velha
+    global matriz
     global jogadas
     os.system("cls")
-    print("   0   1   2")
-    print("0: " + velha[0][0] + " | " + velha[0][1] + " | " + velha[0][2])
-    print(f"   {linha*11}")
-    print("1: " + velha[1][0] + " | " + velha[1][1] + " | " + velha[1][2])
-    print(f"   {linha*11}")
-    print("2: " + velha[2][0] + " | " + velha[0][1] + " | " + velha[0][2])
-    print("Jogadas: "+ Fore.GREEN + str(jogadas) + Fore.RESET)
+    print("     1    2    3")
+    print("1:    " + matriz[0][0] + " | " + matriz[0][1] + " | "+ matriz[0][2])
+    print("    -------------")
+    print("2:    " + matriz[1][0] + " | " + matriz[1][1] + " | "+ matriz[1][2])
+    print("    -------------")
+    print("3:    " + matriz[2][0] + " | " + matriz[2][1] + " | "+ matriz[2][2])
+    print("Jogadas: " + Fore.GREEN + str(jogadas) + Fore.RESET)
 
-def jogoJoga():
+def jogadorJoga():
     global jogadas
     global quemJoga
     global maxJogadas
+
     if quemJoga == 2 and jogadas < maxJogadas:
-        try:
-            l = int(input("Linha..: "))
-            c = int(input("Coluna.: ")) 
-            while velha[l][c] != " ":
+        l = int(input("Linha..: "))
+        l = l-1
+
+        c = int(input("Coluna.: "))
+        c = c-1
+        
+        try:    
+            while matriz[l][c] != " ":
                 l = int(input("Linha..: "))
+                l = l-1
+
                 c = int(input("Coluna.: "))
-            velha[l][c] = "X"
+                c = c-1
+
+            matriz[l][c] = "X"
             quemJoga = 1
-            jogadas += 1
+            jogadas+=1
         except:
-            print("Jogada Inválida")
-            os.system("pause")
-            # vit="n"
-            
+            print("Espaço inválido!")
+
 def cpuJoga():
     global jogadas
-    global quemJoga
     global maxJogadas
-    if quemJoga == 1 and jogadas < maxJogadas:
-        l = random.randrange(0,3)
-        c = random.randrange(0,3)
-        while velha[l][c] != " ":
-            l = random.randrange(0,3)
-            c = random.randrange(0,3)
-        velha[l][c] = "O"
-        jogadas+=1
-        quemJoga=2
+    global quemJoga
 
-def verifyVictory():
-    global velha
-    vitoria="N"
-    simbolos=["X","O"]
+    if quemJoga == 1 and jogadas < maxJogadas:
+        l = random.randrange(0, 3)
+        c = random.randrange(0, 3)
+
+        while matriz [l][c] != " ":
+            l = random.randrange(0, 3)
+            c = random.randrange(0, 3)
+        
+        matriz[l][c] = "O"
+        quemJoga = 2
+        jogadas+=1
+
+def verificarVitoria():
+    global matriz
+    vitoria = "n"
+    simbolos = ["X", "O"]
+
     for s in simbolos:
-        vitoria="N"
-        #Verificar Linhas
-        il=ic=0
-        while il<3:
-            soma=0
-            ic=0
-            while ic<3:
-                if velha[il][ic]==s:
-                    soma += 1
+        vitoria = "n"
+        il = ic = 0
+
+        #verifica linhas
+        while il < 3:
+            soma = 0
+            ic = 0
+            while ic < 3:
+                if(matriz[il][ic] == s):
+                    soma+=1
                 ic+=1
-            if soma == 3:
-                vitoria=s
+            if(soma == 3):
+                vitoria = s
                 break
-        if vitoria != "N":
+            il += 1
+
+        if(vitoria != "n"):
             break
-        vitoria="N"
-        #Verificar Colunas
-        il=ic=0
-        while ic<3:
-            soma=0
-            ic=0
-            while il<3:
-                if velha[il][ic]==s:
-                    soma += 1
-                ic+=1
-            if soma == 3:
-                vitoria=s
+
+        #verifica colunas
+        il = ic = 0
+        while ic < 3:
+            soma = 0
+            il = 0
+            while il < 3:
+                if(matriz[il][ic] == s):
+                    soma+=1
+                il+=1
+            if(soma == 3):
+                vitoria = s
                 break
-            ic+=1
-        if vitoria != "N":
+            ic += 1
+
+        if(vitoria != "n"):
             break
-        vitoria="N"
-        #Verificação de diagonal 1
-        soma=0
-        idiagl=0
-        idiagc=2
-        while idiagc>=0:
-            if velha[idiagl][idiagc]==s:
+
+        #verifica diagonal e-d
+        soma = 0
+        idiag = 0
+
+        while idiag <3:
+            if(matriz[idiag][idiag] == s):
                 soma+=1
-            idiagl+=1
-            idiagc-=1
-        if soma == 3:
-            vitoria=s
+            idiag += 1
+        if(soma == 3):
+            vitoria = s
+            break
+
+        #verifica diagonal d-e
+        soma = 0
+        idiagl = 0
+        idiagc = 2
+        while idiagc >= 0:
+            if(matriz[idiagl][idiagc] == s):
+                soma+=1
+            idiagl += 1
+            idiagc -= 1
+        if(soma == 3):
+            vitoria = s
             break
     return vitoria
-        
+        #verifica diagonal d-e
+
 def redefinir():
-    global velha
+    global matriz
     global jogadas
     global quemJoga
     global maxJogadas
-    global vit
+    global vencedor
     jogadas = 0
-    quemJoga = 2
+    quemJoga = 2 #1 = CPU | #2 = Jogador
     maxJogadas = 9
-    vit = "n"
-    velha = [
-        [" ", " ", " "], #L0C0  L0C1 L0C2
-        [" ", " ", " "], #L1C0  L1C1 L0C2
-        [" ", " ", " "] #L2C0  L2C1 L2C2
+    vencedor = ""
+    matriz = [[" ", " ", " "],[" ", " ", " "],[" ", " ", " "]]
 
-    ]
-        
-while jogarNovamente == "s":      
+
+while(jogarNovamente == "s"):
     while True:
         tela()
-        jogoJoga()
+        jogadorJoga()
         cpuJoga()
-        tela()
-        vit=verifyVictory()
-        if vit != "N" or jogadas>= maxJogadas:
+        vencedor = verificarVitoria()
+        if(vencedor != "n") or (jogadas >= maxJogadas):
             break
 
-    print(Fore.RED + "FIM DE JOGO" + Fore.YELLOW)
-    if vit == "X" or vit == "O":
-        print("Resultado: Jogador " + vit + " Venceu")
+    print(Fore.RED + "Fim de Jogo" + Fore.YELLOW)
+    if vencedor == "X":
+        print("Resultado: Você venceu!")
+    elif vencedor == "O":
+        print("A CPU venceu")
     else:
         print("Resultado: Empate")
-        jogarNovamente = input(Fore.BLUE + "Jogar Novamente?[s/n]:  " + Fore.RESET)
-        redefinir()
-
-        
+    jogarNovamente = input(Fore.BLUE + "Digite [s] para continuar ou [n] para sair: " + Fore.RESET)
