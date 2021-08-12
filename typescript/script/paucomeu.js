c552416f24ba3d1b81895bd8295f3b8225217a44
LuizGafanhotos <Luizfabiano.13lfww@gmail.com>
2021-08-12 17:14:14 -0300

    Dead

---
+++ typescript/script/paucomeu.js
@@ -0,0 +1,28 @@
+//Boolean
+var isOpeno;
+isOpeno = true;
+//string
+var message;
+message = "Ola mundo";
+//Numero
+var total;
+total = 0xff0;
+//Array
+var item;
+item = [1, 2, 3, 4];
+var total2;
+total2 = [1, 2, 3, 4];
+// tuple
+var title;
+title = [12, "Bacana"];
+var Colors;
+(function (Colors) {
+    Colors["white"] = "#fff";
+    Colors["black"] = "#000";
+})(Colors || (Colors = {}));
+var algo;
+algo = "string";
+function logger() {
+    console.log("rar");
+}
+throw new Error("error");
---
+++ typescript/script/paucomeu.ts
@@ -0,0 +1,35 @@
+
+//Boolean
+let isOpeno: boolean
+isOpeno = true
+
+//string
+let message: string
+message = "Ola mundo"
+//Numero
+let total: number
+total = 0xff0
+//Array
+let item: number[]
+item = [1,2,3,4]
+
+let total2: Array<number>
+total2 = [1,2,3,4]
+
+// tuple
+let title: [number,string]
+title = [12,"Bacana"]
+
+enum Colors {
+    white = "#fff",
+    black = "#000"
+}
+let algo
+algo = "string"
+
+function logger(): void {
+    console.log("rar")
+}
+type Bla = string | undefined
+
+throw new Error("error");
