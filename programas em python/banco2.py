55b40b806701feaaa03c664e3b5a23e1a8e90390
LuizGafanhotos <luizfabiano.13lfww@gmail.com>
2021-08-18 15:58:54 -0300

    PEY

---
+++ programas em python/banco2.py
@@ -0,0 +1,32 @@
+from os import error
+import sqlite3
+from sqlite3 import Error
+
+def conector_banco():
+    way = "C:\\Users\\renata\\Documents\\GitHub\MeuDados\\Python_Software\\Banco\\Agendinha.db"
+    con = None
+    try: 
+        con = sqlite3.connect(way)
+    except Error as ex:
+        print("Não há como eu conectar minha pessoa a isso")
+        print(ex)
+    return con
+
+vcon = conector_banco()
+numTelefone = int(input("Digite seu Id de contato: "))
+nome = input("Digite seu nome: ")
+telefone = input("Digite seu telefone: ")
+email = input("Digite seu email: ")
+
+
+vsql = f"INSERT INTO tb_contato VALUES ('{numTelefone}','{nome}','{telefone}','{email}');"
+def inserir(conexao,sql):
+    try:
+        c = conexao.cursor()
+        c.execute(sql)
+        conexao.commit()
+        print("Texto inserido com sucesso")
+    except Error as ex:
+        print(ex)
+
+inserir(vcon,vsql)
