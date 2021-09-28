adf6689f8cdeb970832b286dbfa6fb4124ba34dd
LuizGafanhotos <luizfabiano.13lfww@gmail.com>
2021-09-27 23:44:36 -0300

    Amig

--- programas em python/interface/tk2.py
+++ programas em python/interface/tk2.py
@@ -1,5 +1,8 @@
 from tkinter import *
 
+def impDados():
+    print("oi")
+
 app = Tk()
 app.title("Titulo")
 app.geometry("500x300")
@@ -8,7 +11,11 @@ app.configure(background="#dde")
 
 #Anchor N => norte , S => sul, E =Leste , W = Oeste
 #NE => noroeste, SE = sudeste, SO = sudoeste, NO= noroeste
-Label(app,text="Nome",background="#dde", foreground="#009", anchor=W).place(x=10,y=10,width=100,height=20)
+Label(app,text="Nome",background="#fff", foreground="#009", anchor=W).place(x=10,y=10,width=100,height=20)
+vnome = Entry(app).place(x=10,y=30,width=200, height=20)
 
+Label(app,text="Telefone",background="#fff", foreground="#009", anchor=W).place(x=10,y=60,width=100,height=20)
+vfone = Entry(app).place(x=10,y=80,width=200, height=20)
 
+Button(app,text="Imprimir",command=impDados).place(x=10,y=270,width=100,height=20)
 app.mainloop()
